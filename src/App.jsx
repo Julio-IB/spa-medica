80957e557a43270406fd913479ed192f7a1defb7
JulioRD <ib.julio.ramirez@gmail.com>
2026-08-10 19:04:54 -0600

    Correction for Github deployment (Linux is case sensitive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { Routes, Route } from 'react-router'
 import Home from './components/Home'
-import Services from './pages/services'
-import Location from './pages/location'
+import Services from './pages/Services'
+import Location from './pages/Location'
 
 function App() {
 
