7e454e7ad5eb1615245625f4d109b70de97a9aee
JulioRD <ib.julio.ramirez@gmail.com>
2026-08-11 17:06:32 -0600

    Improvement in the page load time

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import './App.css'
-import { Routes, Route } from 'react-router'
 import Home from './components/Home'
-import Services from './pages/Services'
-import Location from './pages/Location'
 
 function App() {
-
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/servicios' element={<Services />} />
-      <Route path='/ubicacion' element={<Location />} />
-    </Routes>
-  )
+  return <Home />
 }
 
 export default App
